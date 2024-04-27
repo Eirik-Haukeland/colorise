dd1872986c9ea41f20b82f739090a50a70e7c29d
Eirik Berget Haukeland <eirik@haukeland.name>
2024-04-28 00:34:26 +0200

    add function for getting a color based on a key

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,13 @@
 module Lib
     ( someFunc
+    , getColorValue
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+getColorValue :: String -> [([Char], [Char])] -> [Char]
+getColorValue key colorList
+    | null colorList = error "key not found"
+    | fst (head colorList) == key = snd (head colorList)
+    | otherwise = getColorValue key (tail colorList)
