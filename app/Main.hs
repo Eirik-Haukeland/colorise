aef4f7d9672be2c70f24f7f00b8a0cda4eba94a0
Eirik Berget Haukeland <eirik@haukeland.name>
2024-05-09 17:43:27 +0200

    get key from terminal and use it to get color code

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main (main) where
 
 import Lib
+import Colors
+import System.Environment
 
-main :: IO ()
-main = someFunc
+defaultTheme :: String
+defaultTheme = "light"
+
+-- main :: IO ()
+main = do
+    args <- getArgs
+    let ((colorName:res), themeName) = parse (args, defaultTheme)
+    putStrLn (getColorValue colorName (getTheme themeName))
+
+getTheme themeName
+    | themeName == "light" = light
+    | themeName == "dark" = dark
+    | otherwise = light
+
+parse :: ([String], String) -> ([String], String)
+parse ((val:res), themeName)
+    | val == "-l" = parse (res, "light")
+    | val == "--light" = parse (res, "light")
+    | val == "-d" = parse (res, "dark")
+    | val == "--dark" = parse (res, "dark")
+    | otherwise = ([val], themeName)
